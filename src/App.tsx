46ed753ef23366daf68fbd7972323aebae6eb182
Abhishek Kumar <abhixsliet@gmail.com>
2023-10-19 04:39:59 +0530

    App.tsx file added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <main className="grid grid-rows-6 bg-gray-100 w-screen h-screen">
+      <Nav/>
+      <Main />
+    </main>
+  )
+}
+
+export default App;
